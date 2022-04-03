9d57fcf92b55ddda98481bf0c396366be6c7ac1f
saifsms91 <saifsms91@rediffmail.com>
2022-04-04 00:40:55 +0800

    Google Test

---
+++ src/test/java/com/udemy/spring/springselenium/googletest/Google1Test.java
@@ -0,0 +1,32 @@
+package com.udemy.spring.springselenium.googletest;
+
+import com.udemy.spring.springselenium.SpringBaseTestNGTest;
+import com.udemy.spring.springselenium.page.google.GooglePage;
+import com.udemy.spring.springselenium.util.ScreenShotUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Google1Test extends SpringBaseTestNGTest {
+
+    @Autowired
+    private GooglePage googlePage;
+
+    @Lazy
+    @Autowired
+    private ScreenShotUtil screenShotUtil;
+
+    @Test
+    public void googleTest() throws IOException {
+        this.googlePage.goTo();
+        Assert.assertTrue(this.googlePage.isAt());
+        this.googlePage.getSearchComponent().search("spring boot");
+        Assert.assertTrue(this.googlePage.getSearchResult().isAt());
+        Assert.assertTrue(this.googlePage.getSearchResult().getCount()>2);
+     this.screenShotUtil.takeScreenshot();
+        this.googlePage.close();
+    }
+}
